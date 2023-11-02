ce11c9a0b0e55bc29a8c1912c88de9e0207402c7
imranimu <mdimran.khulna@gmail.com>
2023-11-02 19:32:52 +0600

    Create Student Schema and post get method done

--- index.js
+++ index.js
@@ -21,28 +21,56 @@ const mySchema = new Schema({
   age: Number,
 });
 
+const myStudent = new Schema({
+  name: String,
+  email: String,
+  phone: Number,
+  age: Number,
+});
+
 const MyModel = mongoose.model("MyModel", mySchema);
+
+const Student = mongoose.model("Student", myStudent);
 
 app.use(express.json());
 
 app.post("/api/v1/contactform", async (req, res) => {
   const { name, email, phone, age } = req.body;
-//   const data = await MyModel.find({});
-  const createdUser = new MyModel({
-    name,
-    email,
-    phone,
-    age,
-  });
-  await createdUser.save();
-  res.json(createdUser);
+    const createdUser = new MyModel({
+        name,
+        email,
+        phone,
+        age,
+    });
+    await createdUser.save();
+    res.json(createdUser);
+});
+
+app.post("/api/v1/student", async (req, res) => {
+    const { name, email, phone, age } = req.body;  
+    const createdUser = new Student({
+      name,
+      email,
+      phone,
+      age,
+    });
+    await createdUser.save();
+    res.json(createdUser);
 });
 
 app.get("/api/v1/getuser", async (req, res) =>{
     const AllData = await MyModel.find({}); 
 
     res.send(AllData);
-})
+}); 
+
+app.get("/api/v1/getstudents", async (req, res) =>{
+    const AllStudents = await Student.find({}); 
+
+    res.send(AllStudents);
+}); 
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
